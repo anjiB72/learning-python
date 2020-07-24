a16e469e7427537c4074db177c90b43cb9e211d5
Angela Boakes <angela.evans472@gmail.com>
2020-07-24 18:40:16 +0100

    changes middle bit of string conversion code

--- panic.py
+++ panic.py
@@ -3,16 +3,14 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 
-for letter in plist:
-  if letter != 'o' and letter != 'n' and letter != 't' and letter != 'a' and letter != 'p':
-    plist.remove(letter)
-plist.pop()
-plist.pop(5)
-plist.insert(2, ' ')
-plist.insert(4, 'a')
-plist.pop()
-plist.insert(5,'p')
-plist.pop()
+for i in range(4):
+  plist.pop()
+
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2,plist.pop(3))
+            
           
 
 new_phrase = ''.join(plist)
